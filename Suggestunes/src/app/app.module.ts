81a914af820ec84ccb4289ecec637fff96a25135
nharper0792 <78376389+nharper0792@users.noreply.github.com>
2023-03-02 16:36:40 -0500

    added more project files

--- Suggestunes/src/app/app.module.ts
+++ Suggestunes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlaylistComponent } from './playlist/playlist.component';
+import { SongComponent } from './playlist/song/song.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlaylistComponent
+    PlaylistComponent,
+    SongComponent
   ],
   imports: [
     BrowserModule,
