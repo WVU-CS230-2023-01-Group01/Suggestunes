d0d0b06e869b0db6816d5fc38188fcfb5afb50e0
nharper0792 <78376389+nharper0792@users.noreply.github.com>
2023-03-02 20:36:58 -0500

    prepared to create modal for playlist creation

--- Suggestunes/src/app/app.module.ts
+++ Suggestunes/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlaylistComponent } from './playlist/playlist.component';
-import { SongComponent } from './playlist/song/song.component';
+import { PlaylistComponent } from './playlists/playlist/playlist.component';
+import { SongComponent } from './playlists/playlist/song/song.component';
+import { CreatePlaylistComponent } from './playlists/create-playlist/create-playlist.component';
+import { CreatePlaylistModalComponent } from './playlists/create-playlist/create-playlist-modal/create-playlist-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlaylistComponent,
-    SongComponent
+    SongComponent,
+    CreatePlaylistComponent,
+    CreatePlaylistModalComponent
   ],
   imports: [
     BrowserModule,
