8e9d91e5b13e74349393a5c4375e2dfbc2756e6e
noah-marner <ndm00004@mix.wvu.edu>
2023-03-03 18:45:33 -0500

    Login Page Finishedish
    Not sure on the design completely, but basically done kind of

--- Suggestunes/src/app/app.module.ts
+++ Suggestunes/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginBoxComponentComponent } from './login-box-component/login-box-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginBoxComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ Suggestunes/src/app/login-box-component/login-box-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-box-component',
+  templateUrl: './login-box-component.component.html',
+  styleUrls: ['./login-box-component.component.css']
+})
+export class LoginBoxComponentComponent {
+
+}
