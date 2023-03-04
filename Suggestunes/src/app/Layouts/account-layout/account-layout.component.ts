1b57f41f7b209f2f71ac75a12aa04444fe53f146
jmonk28 <jam00056@mix.wvu.edu>
2023-03-04 17:47:40 -0500

    Added "My Friends" component to AccountLayout

--- Suggestunes/src/app/Layouts/account-layout/account-layout.component.ts
+++ Suggestunes/src/app/Layouts/account-layout/account-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input} from '@angular/core';
+import { ProductModel } from './account-layout.component.product_model';
+import {mock_friends_list} from './account-layout.component.mock_friends_list';
 
 @Component({
   selector: 'app-account-layout',
@@ -6,5 +8,11 @@ import { Component, Input} from '@angular/core';
   styleUrls: ['./account-layout.component.css']
 })
 export class AccountLayoutComponent {
+  products: ProductModel[] = [];
 
+  constructor() {
+    for (var product of mock_friends_list) {
+      this.products.push(product);
+    }
+  }
 }
--- Suggestunes/src/app/app.module.ts
+++ Suggestunes/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AccountLayoutComponent } from './Layouts/account-layout/account-layout.component';
 import { NavBarComponent } from './Navigation/nav-bar/nav-bar.component';
 import { AccountInfoComponent } from './Layouts/account-layout/account-info/account-info.component';
+import { AccountFriendsComponent } from './Layouts/account-layout/account-friends/account-friends.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccountLayoutComponent,
     NavBarComponent,
-    AccountInfoComponent
+    AccountInfoComponent,
+    AccountFriendsComponent
   ],
   imports: [
     BrowserModule,
