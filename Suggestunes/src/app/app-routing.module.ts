0a21f63e14b283dbb85251652d5cddae37f10f5f
noah-marner <ndm00004@mix.wvu.edu>
2023-03-05 15:18:01 -0500

    Login page complete with routing

--- Suggestunes/src/app/app-routing.module.ts
+++ Suggestunes/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInLayoutComponent } from './layouts/sign-in-layout/sign-in-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: SignInLayoutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Suggestunes/src/app/app.module.ts
+++ Suggestunes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginBoxComponentComponent } from './login-box-component/login-box-component.component';
+import { SignInLayoutComponent } from './layouts/sign-in-layout/sign-in-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginBoxComponentComponent
+    LoginBoxComponentComponent,
+    SignInLayoutComponent
   ],
   imports: [
     BrowserModule,
