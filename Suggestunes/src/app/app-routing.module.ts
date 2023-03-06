008061101e1233fcf7a38d042546de8eddd5430c
Netherman555 <connorherfurth555@gmail.com>
2023-03-06 14:48:11 -0500

    Added register layout, fixed sign up layout
     - Added the register layout and linked it to the sign up layout.  Currently doesn't do anything.
     - Fixed visual issue with sign up page where it didn't work on some laptops.

--- Suggestunes/src/app/app-routing.module.ts
+++ Suggestunes/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignInLayoutComponent } from './layouts/sign-in-layout/sign-in-layout.component';
+import { SignInLayoutComponent } from './Layouts/sign-in-layout/sign-in-layout.component';
+import { RegisterLayoutComponent } from './Layouts/register-layout/register-layout.component';
 
 const routes: Routes = [
   {
     path:'',
     component: SignInLayoutComponent
+  },
+  {
+    path:'register',
+    component: RegisterLayoutComponent
   }
 ];
 
--- Suggestunes/src/app/app.module.ts
+++ Suggestunes/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginBoxComponentComponent } from './login-box-component/login-box-component.component';
-import { SignInLayoutComponent } from './layouts/sign-in-layout/sign-in-layout.component';
+import { SignInLayoutComponent } from './Layouts/sign-in-layout/sign-in-layout.component';
+import { RegisterLayoutComponent } from './Layouts/register-layout/register-layout.component';
+import { RegisterBoxComponentComponent } from './register-box-component/register-box-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginBoxComponentComponent,
-    SignInLayoutComponent
+    SignInLayoutComponent,
+    RegisterLayoutComponent,
+    RegisterBoxComponentComponent
   ],
   imports: [
     BrowserModule,
