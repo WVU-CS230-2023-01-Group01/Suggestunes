0f90c900bd52758aae5ce13ed478fe5920b7c089
noah-marner <ndm00004@mix.wvu.edu>
2023-03-22 15:48:24 -0400

    Started firebase authentication

--- Suggestunes/src/app/register-box-component/register-box-component.component.ts
+++ Suggestunes/src/app/register-box-component/register-box-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 @Component({
   selector: 'app-register-box-component',
@@ -8,3 +9,16 @@ import { Component } from '@angular/core';
 export class RegisterBoxComponentComponent {
 
 }
+
+const auth = getAuth();
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
