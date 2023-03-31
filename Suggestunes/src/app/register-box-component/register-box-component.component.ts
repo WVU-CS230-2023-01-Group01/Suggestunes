4289f353b5e16fdd63ce4537e22b2e0ca7a5bc23
noah-marner <ndm00004@mix.wvu.edu>
2023-03-31 15:17:14 -0400

    Login and Signup Done

--- Suggestunes/src/app/register-box-component/register-box-component.component.ts
+++ Suggestunes/src/app/register-box-component/register-box-component.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { initializeApp } from '@firebase/app';
 import { createUserWithEmailAndPassword, getAuth } from '@firebase/auth';
 import { getDatabase, ref, set } from '@firebase/database'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-box-component',
@@ -10,103 +11,109 @@ import { getDatabase, ref, set } from '@firebase/database'
 })
 export class RegisterBoxComponentComponent {
 
-  public sayHello() {
-    console.log("Hello World!");
-    return;
+  constructor(private _router: Router) { }
+
+  public callRegister() {
+    var redirect = register()
+    if (redirect == true) {
+      this._router.navigate(['/signin'])
+    }
   }
 }
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAu0Y5ImYX-isiDYT7A3aIPjCIJeoM0yhw",
-//   authDomain: "suggestoons-app.firebaseapp.com",
-//   databaseURL: "https://suggestoons-app-default-rtdb.firebaseio.com",
-//   projectId: "suggestoons-app",
-//   storageBucket: "suggestoons-app.appspot.com",
-//   messagingSenderId: "241615938941",
-//   appId: "1:241615938941:web:4336c2b32cfa1c6a2f5814",
-//   measurementId: "G-EZ8HPMX0KB"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyAu0Y5ImYX-isiDYT7A3aIPjCIJeoM0yhw",
+  authDomain: "suggestoons-app.firebaseapp.com",
+  databaseURL: "https://suggestoons-app-default-rtdb.firebaseio.com",
+  projectId: "suggestoons-app",
+  storageBucket: "suggestoons-app.appspot.com",
+  messagingSenderId: "241615938941",
+  appId: "1:241615938941:web:4336c2b32cfa1c6a2f5814",
+  measurementId: "G-EZ8HPMX0KB"
+};
 
-// const app = initializeApp(firebaseConfig)
-// const auth = getAuth(app)
-// const database = getDatabase(app)
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const database = getDatabase(app)
 
-// function register() {
-//   // Getting input fields
-//   var email = (<HTMLInputElement>document.getElementById("email")).value
-//   var username = (<HTMLInputElement>document.getElementById("username")).value
-//   var password = (<HTMLInputElement>document.getElementById("password")).value
-//   var confirm_password = (<HTMLInputElement>document.getElementById("confirm_password")).value
+function register() {
+  // Getting input fields
+  var email = (<HTMLInputElement>document.getElementById("email")).value
+  var username = (<HTMLInputElement>document.getElementById("username")).value
+  var password = (<HTMLInputElement>document.getElementById("password")).value
+  var confirm_password = (<HTMLInputElement>document.getElementById("confirm_password")).value
 
-//   console.log(email)
-//   // Validate input fields
-//   if (validate_email(email) == false){
-//     alert('Email is not valid.')
-//     return
-//   }
-//   if (validate_password(password) == false){
-//     alert('Password must be greater than 6 characters.')
-//     return
-//   }
-//   if (validate_passwords(password, confirm_password) == false){
-//     alert('Passwords must match.')
-//     return
-//   }
+  // Validate input fields
+  if (validate_email(email) == false) {
+    alert('Email is not valid.')
+    return false
+  }
+  if (validate_password(password) == false) {
+    alert('Password must be greater than 6 characters.')
+    return false
+  }
+  if (validate_passwords(password, confirm_password) == false) {
+    alert('Passwords must match.')
+    return false
+  }
 
-//   // Move on with Auth
-//   createUserWithEmailAndPassword(auth, email, password)
-//   .then(function() {
+  // Move on with Auth
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(function () {
 
-//     var user = auth.currentUser
+      var user = auth.currentUser
 
-//     // Add user to database
-//     var database_ref = ref(database, 'users/' + user!.uid)
+      // Add user to database
+      var database_ref = ref(database, 'users/' + user!.uid)
 
-//     // Create user data
-//     var user_data = {
-//       email : email,
-//       username : username,
-//       last_login : Date.now()
-//     }
+      // Create user data
+      var user_data = {
+        email: email,
+        username: username,
+        last_login: Date.now()
+      }
 
-//     set(database_ref, user_data)
+      set(database_ref, user_data)
 
-//     alert('User Created!')
-//   })
-//   .catch(function(error) {
-//     // Firebase will use this to alert of its errrors
-//     var error_code = error.code
-//     var error_message = error.message
+      alert('User Created!')
+      return true
+    })
+    .catch(function (error) {
+      // Firebase will use this to alert of its errrors
+      var error_code = error.code
+      var error_message = error.message
 
-//     alert(error_message)
-//   })
+      alert(error_message)
+      return false
+    })
 
-// }
+  return true
+}
 
-// function validate_email(email: string) {
-//   const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-//   if (expression.test(email) == true) {
-//     // Email is good
-//     return true
-//   } else {
-//     // Not an email
-//     return false
-//   }
-// }
+function validate_email(email: string) {
+  const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  if (expression.test(email) == true) {
+    // Email is good
+    return true
+  } else {
+    // Not an email
+    return false
+  }
+}
 
-// function validate_password(password: string) {
-//   // for now only checks if password is > 6 characters, can add validation if needed
-//   if (password.length < 6) {
-//     return false
-//   } else {
-//     return true
-//   }
-// }
+function validate_password(password: string) {
+  // for now only checks if password is > 6 characters, can add validation if needed
+  if (password.length < 6) {
+    return false
+  } else {
+    return true
+  }
+}
 
-// function validate_passwords(password: string, confirm_password: string){
-//   if (password == confirm_password){
-//     return true
-//   } else {
-//     return false
-//   }
-// }
+function validate_passwords(password: string, confirm_password: string) {
+  if (password == confirm_password) {
+    return true
+  } else {
+    return false
+  }
+}
