4c6013e4a5820587b3988e17050f4e6368dd1612
Jalen Beeman <97620173+JDB00039@users.noreply.github.com>
2023-04-28 15:48:58 -0400

    Fixed Errors

--- Suggestunes/src/app/app-routing.module.ts
+++ Suggestunes/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes, UrlSegment} from '@angular/router';
+import { RouterModule, Routes, UrlSegment } from '@angular/router';
 import { AccountLayoutComponent } from './layouts/account-layout/account-layout.component';
-import { SignInLayoutComponent } from './Layouts/sign-in-layout/sign-in-layout.component';
-import { RegisterLayoutComponent } from './Layouts/register-layout/register-layout.component';
+import { LandingPageComponent } from './layouts/landing-page/landing-page.component';
+import { ChangeUpLayoutComponent } from './layouts/change-up-layout/change-up-layout.component';
+import { SignInLayoutComponent } from './layouts/sign-in-layout/sign-in-layout.component';
+import { RegisterLayoutComponent } from './layouts/register-layout/register-layout.component';
 import { PlaylistComponent } from './playlists/playlist/playlist.component'
 import { PlaylistHomeLayoutComponent} from './layouts/playlist-home-layout/playlist-home-layout.component'
 import { SpotifyAuthLayoutComponent } from './layouts/spotify-auth-layout/spotify-auth-layout.component'
-import {ChangeUpLayoutComponent} from "./layouts/change-up-layout/change-up-layout.component";
-import {ForgotPasswordComponent} from "./layouts/forgot-password-layout/forgot-password.component";
-import {LandingPageComponent} from "./layouts/landing-page/landing-page.component";
+import { ForgotPasswordComponent } from './layouts/forgot-password-layout/forgot-password.component';
 
 const routes: Routes = [
   {
