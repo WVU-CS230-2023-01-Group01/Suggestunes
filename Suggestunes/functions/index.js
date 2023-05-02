af930dc140ec60f826de7f11be03f2a423b67782
Jalen Beeman <97620173+JDB00039@users.noreply.github.com>
2023-05-02 19:07:08 -0400

    Fixed Merge Conflicts

--- Suggestunes/functions/index.js
+++ Suggestunes/functions/index.js
@@ -50,72 +50,92 @@ const crypto = require('crypto');
 // Set this varibale using 'firebase functions:config:set myapp.secret_token="SECRET_TOKEN"'
 const SECRET_TOKEN = functions.config().webhooks.secrettoken;
 const ACCESS_TOKEN = functions.config().webhooks.accesstoken;
+const API_URL = functions.config().webhooks.apiurl;
 
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiSW50ZWdyYXRpb25BY2Nlc3NUb2tlbiIsInZlcnNpb24iOiIxLjAiLCJpbnRlZ3JhdGlvbklkIjo1MjgsInVzZXJJZCI6Mjc5OTIsImFjY2Vzc1Rva2VuU2VjcmV0IjoiOTMzN2Y4NDJiNTM2MGE5NGM4ZGNjOGJjMzIwNTUyOTA4NDFjNTVmZGU5YzYwYTllMTViNDY4ODdiNWEwMGE1ZCIsImlhdCI6MTY4MzA0MzgyOX0.jcSVXrNMp8OjfRA9MRWhKLBurJb8Wi2uHIY5L8w5gNc
 
 //Secret Token = SECRET_TOKEN
 
 exports.getSuggestion = functions.https.onRequest(async (req, res) => {
-  try {
-    // Split and save important info from the request
-    const body = req.body;
-    const signature = req.headers.signature;
-    const correctSignature = crypto.createHmac("sha512", SECRET_TOKEN).update(JSON.stringify(body)).digest("hex");
+    try {
+        // Split and save important info from the request
+        const body = req.body;
+        const signature = req.headers.signature;
+        const correctSignature = crypto.createHmac("sha512", SECRET_TOKEN).update(JSON.stringify(body)).digest("hex");
 
-    // Print out what the request is
-    console.log("The request is:");
-    console.log(req);
+        // Print out what the request is
+        console.log("The request is:");
+        console.log(req);
 
-    // If either the signature or body do not exist, the request is invalid
-    if (!signature || !body){
-        res.status(400).send('Invalid Request');
-        console.log("Invalid Request" + '\n' + SECRET_TOKEN + '\n' + ACCESS_TOKEN);
-        return;
+        if (!req.body) {
+            console.log('[info] unprocessable entity')
+            res.status(422).send('Invalid Request - No Body');
+        }
+
+        console.log("[info] incoming event:");
+        console.log(JSON.stringify(req.body, undefined, 2));
+
+        if (req.body.type === "TEST") {
+            console.log("[info] processing test event");
+            res.status(200).send('Processing Test Event');
+        }
+
+        // If either the signature or body do not exist, the request is invalid
+        if (!signature) {
+            res.status(400).send('Invalid Request - No Signature' + '\n' + SECRET_TOKEN + '\n' + ACCESS_TOKEN);
+            console.log("Invalid Request");
+            return;
+        }
+        console.log("Request had both signature and body");
+
+        // If the signure is not correct, spit out unauthorized request response
+        if (signature !== correctSignature) {
+            res.status(401).send('Unauthorized Request');
+        }
+
+        if (req.body.event.type === "AudioAnalysisV6" && req.body.event.status === "finished") {
+            console.log("[info]")
+            asynchronouslyFetchspotifyTrackResult(req.body.resource.id);
+        }
+        // Your webhook logic goes here, using the payload object
+        res.status(200).send('Webhook processed successfully');
+    } catch (error) {
+        console.log(req);
+        console.error(error);
+        res.status(500).send('An error occurred while processing the webhook' + '\n' + error + '\n' + req + '\n' + functions.config().webhooks.secrettoken);
     }
-    console.log("Request had both signature and body");
-
-    // If the signure is not correct, spit out unauthorized request response
-    if (signature !== correctSignature){
-        res.status(401).send('Unauthorized Request');
-    }
-
-    if (req.body.event.type === "AudioAnalysisV6" && req.body.event.status === "finished") {
-        console.log("[info]")
-    }
-    // Your webhook logic goes here, using the payload object
-    res.status(200).send('Webhook processed successfully');
-  } catch (error) {
-    console.log(req);
-    console.error(error);
-    res.status(500).send('An error occurred while processing the webhook' + '\n' + error +'\n' + req + '\n' + functions.config().webhooks.secrettoken);
-  }
 });
 
-
-const fetch = require("node-fetch");
-const API_URL = '';
-
-exports.sendRequest = functions.https.onRequest((_req, res) => {
-const libraryTrackEnqueue = async libraryTrackId => {
-    const mutationDocument = /* GraphQL */ `
-    query SimilarTracksQuery($trackId: ID!) {
-        libraryTrack(id: $trackId) {
-          __typename
-          ... on Error {
+const asynchronouslyFetchspotifyTrackResult = async spotifyTrackId => {
+    // fetch the whole information
+    const spotifyTrackQueryDocument = /* GraphQL */ `
+      query SpotifyTrack($spotifyTrackId: ID!) {
+        spotifyTrack(id: $spotifyTrackId) {
+          ... on SpotifyTrackNotFoundError {
             message
           }
-          ... on Track {
+          ... on SpotifyTrack {
             id
-            similarTracks(target: { spotify: {} }) {
-              __typename
-              ... on SimilarTracksError {
-                code
+            audioAnalysisV6 {
+              ... on AudioAnalysisV6Finished {
+                result {
+                  genre {
+                    ambient
+                    blues
+                  }
+                }
+              }
+            }
+            similarSpotifyTracks {
+              ... on SimilarSpotifyTracksError {
                 message
               }
-              ... on SimilarTracksConnection {
+              ... on SimilarSpotifyTrackConnection {
                 edges {
                   node {
-                    id
+                    spotifyTrack {
+                      id
+                    }
                   }
                 }
               }
@@ -142,4 +162,45 @@ const libraryTrackEnqueue = async libraryTrackId => {
   
     return result.data;
   };
+
+const fetch = require("node-fetch");
+
+exports.sendRequest = functions.https.onRequest((_req, res) => {
+const libraryTrackEnqueue = async libraryTrackId => {
+    const mutationDocument = /* GraphQL */ `
+    query SimilarTracksQuery($trackId: ID!) {
+        libraryTrack(id: $trackId) {
+          __typename
+          ... on Error {
+            message
+          }
+          ... on Track {
+            id
+            similarTracks(target: { spotify: {} }) {
+              __typename
+              ... on SimilarTracksError {
+                code
+                message
+              }
+              ... on SimilarTracksConnection {
+                edges {
+                  node {
+                    id
+      }  
+    `;
+
+    const result = await fetch(API_URL, {
+        method: "POST",
+        body: JSON.stringify({
+            query: spotifyTrackQueryDocument,
+            variables: { spotifyTrackId }
+        }),
+        headers: {
+            Authorization: "Bearer " + ACCESS_TOKEN,
+            "Content-Type": "application/json"
+        }
+    }).then(res => res.json());
+    console.log("[info] spotifyTrack result");
+    console.log(JSON.stringify(result, undefined, 2));
+};
 });
